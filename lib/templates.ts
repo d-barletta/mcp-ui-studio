0fb7b922c088fc203ce2bd9b93b5ac34b211d27b
DB <51680747+d-barletta@users.noreply.github.com>
2025-12-22 22:06:28 +0100

    Remove unnecessary newline in templates re-export

--- lib/templates.ts
+++ lib/templates.ts
@@ -1,4 +1,3 @@
 // Re-export all templates from the templates folder
 // Each category has its own dedicated file in lib/templates/
 export { templates, dashboardTemplates, formTemplates } from './templates/index';
-
